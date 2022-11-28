f63282384bde48db51f4658806dbc5db6d456f31
Thomas <thomasjprice2@gmail.com>
2022-11-28 18:11:45 +0000

    Notify Theming

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const NavLinks = ({ handleClick }) => (
       <NavLink
       key={item.name}
       to={item.to}
-      className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400'
+      className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-[#1ab26b]'
       onClick={() => handleClick && handleClick()}
       >
         <item.icon className="w-6 h-6 mr-2"/>
@@ -27,8 +27,8 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#044124]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain scale-150" />
         <NavLinks />
       </div>
 
